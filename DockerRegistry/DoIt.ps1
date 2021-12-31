8914e9d6198ae8c4cf099ac2a400001e5035e7d4
Thierry <Thierry.Brunet@outlook.com>
2022-01-01 07:24:16 +1000

    Initial commit

---
+++ DockerRegistry/DoIt.ps1
@@ -0,0 +1,182 @@
+﻿#region -- [1] Docker Hub Registry
+#-----------------------------
+https://hub.docker.com/repositories  # Thierry's repo
+
+docker login
+docker login --username=thierrybrunet --email=Thierry.Brunet@outlook.com
+docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+
+# As far as I know you have to use docker login. The credentials will be stored in /home/user/.docker/config.json for following docker pushes.
+docker login -u ${USERNAME} -p ${PASSWORD} ${DOCKER_REPOSITORY}
+
+docker images
+# redis                                latest                                                  7614ae9453d1   8 days ago      113MB
+# openzipkin/zipkin                    latest                                                  0614d392dbc2   10 days ago     155MB
+# daprio/dapr                          1.5.1                                                   226aa2a3ceeb   3 weeks ago     214MB
+# k8s.gcr.io/etcd                      3.4.13-0                                                0369cf4303ff   16 months ago   253MB
+
+# SAVE
+docker save -o c:\docker\etcd.tar ek8s.gcr.io/etcd:3.4.13-0  # not working
+docker save -o c:\docker\etcd.tar 0369cf4303ff
+docker save -o c:\docker\redi.tar redis:latest
+docker save -o c:\docker\daprio.tar  daprio/dapr:1.5.1
+docker save -o c:\docker\zipkin.tar  openzipkin/zipkin:latest
+
+# push REDIS -- redis:latest
+docker tag redis:latest thierrybrunet/opec-redis:v1
+docker push thierrybrunet/opec-redis:v1
+
+docker pull thierrybrunet/opec-redis:v1
+
+
+# push ZIPKIN -- openzipkin/zipkin:latest
+docker tag openzipkin/zipkin:latest thierrybrunet/opec-zipkin:v1
+docker push thierrybrunet/opec-zipkin:v1
+
+docker pull thierrybrunet/opec-zipkin:v1
+
+
+# push daprio -- daprio/dapr:1.5.1
+docker tag daprio/dapr:1.5.1 thierrybrunet/opec-daprio:v1
+docker push thierrybrunet/opec-daprio:v1
+
+docker pull thierrybrunet/opec-daprio:v1
+
+
+# push ETCD -- k8s.gcr.io/etcd:3.4.13-0
+docker tag k8s.gcr.io/etcd:3.4.13-0 thierrybrunet/opec-etcd:v1
+docker push thierrybrunet/opec-etcd:v1
+
+docker pull thierrybrunet/opec-etcd:v1
+
+#endregion
+
+#region -- [2] Redhat Quay Registry
+#----------------------------------
+https://quay.io/repository/
+
+docker login quay.io --username=thierrybrunet_1 # --password=7Z[
+# Username: thierrybrunet_1
+# Password: ( password here)
+# Email: thierry.brunet@outlook.com
+
+
+# push REDIS -- redis:latest
+docker tag redis:latest quay.io/thierrybrunet_1/opec-redis:v1
+docker push quay.io/thierrybrunet_1/opec-redis:v1
+
+docker pull quay.io/thierrybrunet_1/opec-redis:v1
+
+
+# push ZIPKIN -- openzipkin/zipkin:latest
+docker tag openzipkin/zipkin:latest quay.io/thierrybrunet_1/opec-zipkin:v1
+docker push quay.io/thierrybrunet_1/opec-zipkin:v1
+
+docker pull quay.io/thierrybrunet_1/opec-zipkin:v1
+
+
+# push daprio -- daprio/dapr:1.5.1
+docker tag daprio/dapr:1.5.1 quay.io/thierrybrunet_1/opec-daprio:v1
+docker push quay.io/thierrybrunet_1/opec-daprio:v1
+
+docker pull quay.io/thierrybrunet_1/opec-daprio:v1
+
+
+# push ETCD -- k8s.gcr.io/etcd:3.4.13-0
+docker tag k8s.gcr.io/etcd:3.4.13-0 quay.io/thierrybrunet_1/opec-etcd:v1
+docker push quay.io/thierrybrunet_1/opec-etcd:v1
+
+docker pull quay.io/thierrybrunet_1/opec-etcd:v1
+
+
+# Docker commit
+docker commit 07f2065197ef quay.io/thierrybrunet_1/myfirstrepo
+docker push quay.io/thierrybrunet_1/opec-dapr
+
+#endregion
+
+#region -- [3] Bitnami ETCD
+#--------------------------
+https://hub.docker.com/r/bitnami/etcd/
+https://github.com/bitnami/bitnami-docker-etcd  #how-to-use-this-image
+
+docker pull bitnami/etcd:latest
+docker images
+# bitnami/etcd   latest   21887a7c9ed7   9 hours ago     150MB
+
+# Step 1: Create a network
+$ docker network create app-tier --driver bridge
+
+# Step 2: Launch the Etcd server instance
+# Use the --network app-tier argument to the docker run command to attach the Etcd container to the app-tier network.
+
+$ docker run -d --name Etcd-server \
+    --network app-tier \
+    --publish 2379:2379 \
+    --publish 2380:2380 \
+    --env ALLOW_NONE_AUTHENTICATION=yes \
+    --env ETCD_ADVERTISE_CLIENT_URLS=http://etcd-server:2379 \
+    bitnami/etcd:latest
+
+# Step 3: Launch your Etcd client instance
+# Finally we create a new container instance to launch the Etcd client and connect to the server created in the previous step:
+
+$ docker run -it --rm \
+    --network app-tier \
+    --env ALLOW_NONE_AUTHENTICATION=yes \
+    bitnami/etcd:latest Etcdctl --endpoints http://etcd-server:2379 put /message Hello
+
+
+# Configuration
+# The configuration can easily be setup by mounting your own configuration file on the directory /opt/bitnami/etcd/conf:
+
+$ docker run --name Etcd -v /path/to/Etcd.conf.yml:/opt/bitnami/Etcd/conf/etcd.conf.yml bitnami/etcd:latest
+
+# Using Docker Compose
+# When not specified, Docker Compose automatically sets up a new network and attaches all deployed services to that
+# network. However, we will explicitly define a new bridge network named app-tier. In this example we assume that
+# you want to connect to the Etcd server from your own custom application image which is identified in the following snippet by the service name myapp.
+
+version: '2'
+
+networks:
+  app-tier:
+    driver: bridge
+
+services:
+  Etcd:
+    image: 'bitnami/etcd:latest'
+    environment:
+      - ALLOW_NONE_AUTHENTICATION=yes
+      - ETCD_ADVERTISE_CLIENT_URLS=http://etcd:2379
+    ports:
+      - 2379:2379
+      - 2380:2380
+    networks:
+      - app-tier
+    volumes:
+      - /path/to/Etcd.conf.yml:/opt/bitnami/etcd/conf/etcd.conf.yml
+  myapp:
+    image: 'YOUR_APPLICATION_IMAGE'
+    networks:
+      - app-tier
+
+# IMPORTANT:
+# Please update the placeholder YOUR_APPLICATION_IMAGE in the above snippet with your application image
+# In your application container, use the hostname etcd to connect to the Etcd server
+
+# Launch the containers using:
+$ docker-compose up -d
+#endregion
+
+#region -- [4] ETCD CoreOs
+#-------------------------
+
+https://quay.io/repository/coreos/etcd?tab=tags
+
+
+docker pull quay.io/coreos/etcd:v3.5.1-arm64
+
+#endregion
+
+
---
+++ dapr_offline_install/prestage.sh
@@ -0,0 +1,129 @@
+#!/usr/bin/env bash
+
+
+# WARNING: The directory placed here will be subject to a rm -rf operation.
+# See prepare_prestaged_directory() for details.
+PRESTAGE_DIRECTORY='./prestaged'
+
+
+get_dapr_cli() {
+    echo -e "\nFetching https://github.com/dapr/cli/releases/download/v${DAPR_VERSION}/dapr_${OS}_${ARCH}.tar.gz..."
+    curl -L https://github.com/dapr/cli/releases/download/v${DAPR_VERSION}/dapr_${OS}_${ARCH}.tar.gz -o ${PRESTAGE_DIRECTORY}/dapr_${OS}_${ARCH}.tar.gz
+}
+
+
+get_daprd() {
+    echo -e "\nFetching https://github.com/dapr/dapr/releases/download/v${DAPR_VERSION}/daprd_${OS}_${ARCH}.tar.gz..."
+    curl -L https://github.com/dapr/dapr/releases/download/v${DAPR_VERSION}/daprd_${OS}_${ARCH}.tar.gz -o ${PRESTAGE_DIRECTORY}/daprd_${OS}_${ARCH}.tar.gz
+}
+
+
+get_placement() {
+    echo -e "\nFetching https://github.com/dapr/dapr/releases/download/v${DAPR_VERSION}/placement_${OS}_${ARCH}.tar.gz..."
+    curl -L https://github.com/dapr/dapr/releases/download/v${DAPR_VERSION}/placement_${OS}_${ARCH}.tar.gz -o ${PRESTAGE_DIRECTORY}/placement_${OS}_${ARCH}.tar.gz
+}
+
+
+set_options() {
+    OS="linux"
+
+    if [ -z "${ARCH}" ]; then
+        ARCH=$(uname -m)
+    fi
+
+    if [ -z "${DAPR_VERSION}" ]; then
+        dapr_release_url="https://api.github.com/repos/dapr/dapr/releases"
+        DAPR_VERSION=$(curl -s ${dapr_release_url} | grep \"tag_name\" | grep -v rc | awk 'NR==1{print $2}' |  sed -n 's/\"v\(.*\)\",/\1/p')
+    fi
+
+    if [ -z ${SLIM_INIT} ]; then
+        SLIM_INIT=0
+    fi
+
+    case $ARCH in
+        aarch64) ARCH="arm64";;
+        amd64) ARCH="amd64";;
+        arm64) ARCH="arm64";;
+        x86_64) ARCH="amd64";;
+        \?)
+            echo "Unsupported architecture: ${ARCH}"
+            exit 1;;
+    esac
+
+    echo -e "Prestaging dapr installation for:\n"
+    echo -e "Architecture: ${ARCH}"
+    echo -e "OS: ${OS}"
+    echo -e "Dapr Version: ${DAPR_VERSION}"
+}
+
+
+prepare_prestaged_directory() {
+    rm -rf ${PRESTAGE_DIRECTORY}
+    mkdir -p ${PRESTAGE_DIRECTORY}
+    cp -r ./components ${PRESTAGE_DIRECTORY}
+    cp config.yaml ${PRESTAGE_DIRECTORY}
+}
+
+
+prestage_dapr_image() {
+    echo ""
+    docker pull --platform ${OS}/${ARCH} daprio/dapr:${DAPR_VERSION}
+    docker save daprio/dapr:${DAPR_VERSION} -o ${PRESTAGE_DIRECTORY}/dapr_placement.tar
+}
+
+
+prestage_redis_image () {
+    # Currently only supports linux/amd64 and linux/arm64
+    echo ""
+    if [ ${OS} == "arm64" ]; then
+        docker pull --platform ${OS}/${ARCH}/v8 redis:latest
+    else  # amd64
+        docker pull --platform ${OS}/${ARCH} redis:latest
+    fi
+    docker save redis:latest -o ${PRESTAGE_DIRECTORY}/redis.tar
+}
+
+
+prestage_zipkin_image () {
+    # Currently only supports linux/amd64 and linux/arm64
+    echo ""
+    docker pull --platform ${OS}/${ARCH} openzipkin/zipkin:latest
+    docker save openzipkin/zipkin:latest -o ${PRESTAGE_DIRECTORY}/zipkin.tar
+}
+
+
+prestage_docker_images() {
+    prestage_dapr_image
+    prestage_redis_image
+    prestage_zipkin_image
+}
+
+
+prestage_slim_init() {
+    echo "Prestaging for Slim Init"
+    rm -rf ${PRESTAGE_DIRECTORY}
+    mkdir -p ${PRESTAGE_DIRECTORY}
+    get_dapr_cli
+    get_daprd
+    get_placement
+}
+
+
+while getopts "a:sv:" flag; do
+    case ${flag} in
+        a) ARCH=${OPTARG};;
+        s) SLIM_INIT=1;;
+        v) DAPR_VERSION=${OPTARG};;
+        \?) exit 1;
+    esac
+done
+
+set_options
+if [ ${SLIM_INIT} == 1 ]; then
+    prestage_slim_init
+else
+    prepare_prestaged_directory
+    prestage_docker_images
+    get_dapr_cli
+    get_daprd
+fi
